59d33d11370b6d86c645742590266d6aab9d3103
Jordan Bruner <jordanbruner@Jordans-MacBook-Pro.local>
2019-11-25 09:38:24 -0700

    market and images

--- src/Market/index.js
+++ src/Market/index.js
@@ -4,6 +4,8 @@ import {Modal, Button} from 'semantic-ui-react';
 const Market = (props) => {
     return(
         <Modal open={props.open}>
+
+        <img src={require('/testImages/potions(5).png')} /><img src={require('/testImages/potions(4).png')} /><img src={require('/testImages/potions(3).png')} /> <br/>
         <button type="button" onClick={() => props.createItem(1)}>purchase minor potion</button>
         <button type="button" onClick={() => props.createItem(2)}>purchase potion</button>
         <button type="button" onClick={() => props.createItem(3)}>purchase super potion</button><br/>
